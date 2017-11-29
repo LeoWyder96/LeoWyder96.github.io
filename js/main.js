e152c37d222083d3a73742afb29c38e54ac63c4e
LeoWyder96 <leowyder@gmail.com>
2017-11-29 17:49:16 -0600

    Final Project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.radioSelect').slick({
+        centerMode: true,
+        centerPadding: '10px',
+        infinite: true,
+        slidesToShow: 3,
+        asNavFor: '.radioName'
+	});
+});
+$(document).ready(function(){
+	$('.radioName').slick({
+        slidesToShow: 1,
+  		slidesToScroll: 1,
+  		arrows: false,
+  		fade: true,
+	});
+});
---
+++ test1.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+      $('.radioSelect').slick({
+        dots: true,
+        centerMode: true,
+        centerPadding: '60px',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      });
+    });
