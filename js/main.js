e1755c0b427b9fa02d8365ba9e24cc155815a64d
LeoWyder96 <leowyder@gmail.com>
2017-12-04 14:48:03 -0600

    Added raido control buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var audioPlayer1 = document.getElementById("audio-player1");
+var audioPlayer2 = document.getElementById("audio-player2");
+var audioPlayer3 = document.getElementById("audio-player3");
+
 $(document).ready(function(){
 	$('.radioSelect').slick({
         centerMode: true,
@@ -17,20 +21,32 @@ $(document).ready(function(){
 });
 
 
-// $(".selectOne").hide(1);
+$(".selectOne").hide(1);
 $(".selectTwo").hide(1);
 $(".selectThree").hide(1);
 
 
 $(".radioItem1").on("click", function(){
+
   $(".selectOne").hide(1);
   $(".selectTwo").hide(1);
   $(".selectThree").hide(1);
 
   $(".selectOne").slideDown(400);
+  if (audioPlayer1.play) {
+    audioPlayer1.pause();
+  }
+  if (audioPlayer2.play) {
+    audioPlayer2.pause();
+  }
+  if (audioPlayer3.play) {
+    audioPlayer3.pause();
+  }
 
 });
 $(".radioItem2").on("click", function(){
+  
+
   $(".selectOne").hide(1);
   $(".selectTwo").hide(1);
   $(".selectThree").hide(1);
